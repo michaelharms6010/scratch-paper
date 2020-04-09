9619e2a828ff8e8c8777f973a0c51afc397814a7
Michael Harms <michael.harms6010@gmail.com>
2020-04-09 17:26:18 -0500

    minswaps

--- scratch.js
+++ scratch.js
@@ -1,3 +1,11 @@
+function minSwaps(s1, s2) {
+	let count = 0;
+	for (let i = 0; i < s1.length; i++) {
+		if (s1[i] !== s2[i]) count += 1
+	}
+	return count / 2
+}
+
 function rootDigit(n) {
 	let numarr = String(n).split("").map(item => Number(item))
 	while (numarr.length > 1) {
