c335aad02150742871d2b9b3f9b4d74e3d52899a
Michael Harms <michael.harms6010@gmail.com>
2020-06-11 08:31:48 -0500

    classic brackets

--- scratch.rb
+++ scratch.rb
@@ -11,3 +11,30 @@ end
 def numArgs(*args, **kwargs, &blk)
     args.size + kwargs.size + (blk.nil? ? 0: 1)
 end
+
+def bracket_logic(xp)
+	pairs = {
+		"[" => "]",
+		"(" => ")",
+		"<" => ">",
+		"{" => "}"
+	}
+	stack = []
+    xp.chars.each do |i|
+		if pairs.keys.include? i
+			stack.push(i)
+		elsif pairs.values.include? i
+			return false if i != pairs[stack.pop()]
+		end
+    end
+    stack.length == 0
+end
+
+
+def xmas_items(n)
+	total = 0
+	for i in (0..n) do
+		total += i
+	end
+	total
+end
